194f49170cf69fdc7f823d80391c1e37195a4793
stensoomre <98737965+stensoomre@users.noreply.github.com>
2022-03-22 20:35:48 +0200

    Add files via upload

--- Salvestamine.py
+++ Salvestamine.py
@@ -1,6 +1,5 @@
-from time import *
 from random import *
-import re
+from time import *
 
 nimi = input("Siseta nimi: ")
 
@@ -20,10 +19,16 @@ ruu_def = randint(1,300)
 playerdef += ruu_def
 aksessuaar = input("Sisesta aksessuaari nimetus (string): ")
 aksessuaar_info = "\"Mdea sa oled pede\""
+playerlevel = randint(1,100)
+xp_needed = round(randint(10,1000)*(playerlevel*1.275),0)
+xp_current = round(randint(10,1000),0)
+if xp_current>xp_needed:
+    xp_current -= xp_needed
 
 
 f= open(f"{nimi}.txt","w+", encoding="UTF-8")
 f.write(f"Nimi: {nimi}\n")
+f.write(f"Level: {playerlevel} ({xp_current}XP/{xp_needed}XP)\n")
 f.write("---------------------\n")
 f.write(f"ATK: {playerattack}\n")
 f.write(f"DEF: {playerdef}\n")
@@ -37,3 +42,6 @@ f.write(f"RELV: {relv} ({relv_min}-{relv_max})\n")
 f.write(f"RÜÜ: {ruu} +{ruu_def} DEF\n")
 f.write(f"AKSESSUAAR: {aksessuaar} {aksessuaar_info}\n")
 f.close()
+sleep(1)
+print("")
+print(f"Salvestatud {nimi}.txt'ina")
